4f77ea791540080d2e63cb5fc1beef34f6c4c5ab
Student <example@example.com>
2020-02-14 14:31:41 -0800

    add API key

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,8 +1,13 @@
 export class Doctor {
   async getDoctorByName(name) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&${process.env.API_KEY}`)
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      // console.log(jsonifiedResponse);
+      return jsonifiedResponse;
+      
+    } catch(error) {
+      console.error("There was an error locatin a doctor. Please try again!" + error.message);
     }
-    
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,3 +3,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Doctor } from './../src/doctor-service.js'
+
+$(document).ready(function() {
+  $("#locator").submit(function(event) {
+    event.preventDefault();
+    const name = $("#symptoms").val();
+    $("#symptoms").val("");
+
+    asyncApiCall();
+
+    async function asyncApiCall() {
+      try {
+        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&${name}user_location=37.773%2C-122.413&skip=0&limit=10&${process.env.API_KEY}`);
+        let jsonifiedResponse;
+        if (response.ok && response.status == 200) {
+          jsonifiedResponse = await response.json();
+        } else {
+          jsonifiedResponse = false;
+        }
+        getElements(jsonifiedResponse);
+      } catch {
+        getElements(false);
+      }
+    }
+
+
+
+    
+  });
+});
