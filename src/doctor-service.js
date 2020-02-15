f31e37a7c3c12e1d14dc236ea58f46235ac6d1bd
Student <example@example.com>
2020-02-14 18:01:39 -0800

    add async functions to user interface.

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -18,9 +18,9 @@ export class DoctorInfo {
 }
 
 export class UserSymptom {
-  async getUserBySymptom(symptoms) {
+  async getUserBySymptom(sympt) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${sympt}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse; 
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -2,3 +2,53 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { DoctorInfo } from './../src/doctor-service.js';
+import { UserSymptom } from './../src/doctor-service.js';
+
+$(document).ready(function() {
+  $("#userSympt").submit(function(event) {
+    event.preventDefault();
+    const sympt = $("#symptoms").val();
+    $("#symptoms").val("");
+
+    (async () => {
+      let userSymptom = new UserSymptom();
+      const response1 = await userSymptom.getUserBySymptom(sympt);
+      getElements(response1);
+    })();
+    
+    function getElements(response1) {
+      if (response1 === false) {
+        $("#result").prepend("There was an error locatin a doctor. Please try again!")
+        
+      }
+    }
+
+    $("#showName").prepend(`<li> ${name}  </li>`);
+    
+  });
+});
+
+
+$(document).ready(function() {
+  $("#info").submit(function(event) {
+    event.preventDefault();
+    const name = $("#docInfo").val();
+    $("#docInfo").val("");
+
+    (async () => {
+      let doctorInfo = new DoctorInfo();
+      const response2 = await doctorInfo.getDoctorByName(name);
+    //   getElements(response2);
+    })();
+    
+    // function getElements(response1) {
+    //   if (response1) {
+
+    //   }
+    // }
+
+    // $("#showName").prepend(`<li> ${name}  </li>`)
+    
+  });
+  });
