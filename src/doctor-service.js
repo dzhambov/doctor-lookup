a5e620a2c83272c632b4b5a4fc7f96596350a30b
Student <example@example.com>
2020-02-22 18:05:47 -0800

    add functionality for symptoms or doctor search from the same input button.

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -2,7 +2,6 @@ export class Doctor {
   async getDoctorByName(query) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=${process.env.API_KEY}`);  
-      console.log(response);
       
       let jsonifiedResponse; 
       if (response.ok && response.status == 200) {
--- src/main.js
+++ src/main.js
@@ -3,32 +3,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Doctor } from './../src/doctor-service.js';
-// import { UserSymptom } from './../src/doctor-service.js';
-
-// $(document).ready(function() {
-//   $("#userSympt").submit(function(event) {
-//     event.preventDefault();
-//     const sympt = $("#symptoms").val();
-//     $("#symptoms").val("");
-//     console.log(sympt);
-    
-//     (async () => {
-//       let userSymptom = new UserSymptom();
-//       const response1 = await doctor.getUserBySymptom(sympt);
-//       getElements(response1);
-//     })();
-    
-//     function getElements(response1) {
-//       if (response1 === false) {
-//         $("#result").prepend("There was an error locatin a doctor. Please try again!");
-        
-//       }
-//     }
-
-//     $("#showName").prepend(`<li> ${name}  </li>`);
-    
-//   });
-// });
 
 
 $(document).ready(function() {
@@ -54,7 +28,7 @@ $(document).ready(function() {
       } else if (response2.data.length > 0) {
         response2.data.forEach(function(doc) {
           docArr.push(response2);
-          $("#doctorInformation").prepend(`<li> Doctor's Name: ${doc.profile.title} ${doc.profile.first_name} ${doc.profile.last_name} <br> Accepts New Patients: ${doc.practices[0].accepts_new_patients} <br> Address: ${doc.practices[0].visit_address.street} <br> ${doc.practices[0].visit_address.city}, ${doc.practices[0].visit_address.state}, ${doc.practices[0].visit_address.zip} <br> Phone: ${doc.practices[0].phones[0].number}</li>`);
+          $("#doctorInformation").prepend(`<li> Doctor's Name: ${doc.profile.title} ${doc.profile.first_name} ${doc.profile.last_name} <br> Accepts New Patients: ${doc.practices[0].accepts_new_patients} <br> Address: ${doc.practices[0].visit_address.street} <br> ${doc.practices[0].visit_address.city}, ${doc.practices[0].visit_address.state}, ${doc.practices[0].visit_address.zip} <br> Phone: ${doc.practices[0].phones[0].number} <br> Website: <a href="${doc.practices[0].website}"</a> </li>`);
         });
         
       }
